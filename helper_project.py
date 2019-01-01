6dc6144e0bb3a184d18150b73eb638c81b1e4cb3
kshashankrao <shashank.kadapanatham@gmail.com>
2019-01-01 07:14:37 +0100

    second commit

--- helper_project.py
+++ helper_project.py
@@ -6,11 +6,9 @@ import random
 import numpy as np
 from random import shuffle
 from sklearn.model_selection import train_test_split
-data=[]
-labels = []
-basepath = "D:/DeepLearning/PCL_Segmentation/point_net/pointnet/del"
-##basepath = "D:/DeepLearning/PCL_Segmentation/point_net/pointnet/mesh_sampling/build/data/"
-#p =  pcl.PointCloud()
+
+basepath = "D:/DeepLearning/PCL_Segmentation/point_net/pointnet/dell"
+#basepath = "D:/DeepLearning/PCL_Segmentation/point_net/pointnet/mesh_sampling/build/data/"
 
 def rename(path):
     num = 0
@@ -32,6 +30,8 @@ def list_files(basePath, validExts=None, contains=None):
                 yield imagePath
 
 def convert2h5():
+        data=[]
+        labels = []
         pcdPaths = sorted((list(list_files(basepath,validExts=".pcd",contains=None))))
         random.seed(42)
         random.shuffle(pcdPaths)
@@ -46,7 +46,13 @@ def convert2h5():
                 for i in range(offset):
                     point_cloud_np = np.vstack([point_cloud_np,point_cloud_np[-1]])
                 print("Corrected Shape: "+str(point_cloud_np.shape))
-            data.append(point_cloud_np)
+            points = point_cloud_np
+            centroid = np.mean(points, axis=0)
+            points -= centroid
+            furthest_distance = np.max(np.sqrt(np.sum(abs(points)**2,axis=-1)))
+            points /= furthest_distance
+            print(points)
+            data.append(points)
             labelName = pcdPath.split("\\")[-1].split("_")[-2]
             print(labelName+"\n")
             if(labelName == "car"):
@@ -54,20 +60,19 @@ def convert2h5():
             else:
                 label = 1
             # Never Do this label = 1
-            #print("Class: "+labelName+"/n"+"Label: "+str(label)+"/n")
+            print("Class: "+labelName+"\n"+"Label: "+str(label)+"\n")
             labels.append(label)
-
         labelss = np.asarray(labels)
         labelss.shape = [labelss.shape[0],1]
         print(labelss)
         #split = train_test_split(data, labelss,test_size=0.2, random_state=42)
         #trainX, trainY, labelX, labelY = split
-
-        hf_train = h5py.File('test_data.h5','w')
+        print(np.array(data).shape)
+        hf_train = h5py.File('Data_test.h5','w')
         hf_train.create_dataset('data',data=data)
         hf_train.create_dataset('label',data=labelss)
         hf_train.close()
-        print(labelss.shape )
+        #print(labelss.shape)
         # hf_val = h5py.File('test_data.h5','w')
         # hf_val.create_dataset('data',data=trainY)
         # hf_val.create_dataset('label',data=labelY)
@@ -77,8 +82,19 @@ def loadh5(filename):
     f = h5py.File(filename)
     data = f['data'][:]
     label = f['label'][:]
-    print (label)
+    print (data)
 
-#loadh5("train.h5")
+def h5toarray(filename):
+    hf = h5py.File(filename, 'r')
+    data = hf.get('data').value
+    print(data)
+    p =  pcl.PointCloud()
+    pcd_new = p.from_array(data[0])
+    pcl.save(p,"test.pcd")
+
+
+
+#loadh5("delete_now.h5")
 #rename("D:/DeepLearning/PCL_Segmentation/point_net/pointnet/mesh_sampling/build/data/car_train")
 convert2h5()
+#h5toarray("delete_now.h5")
